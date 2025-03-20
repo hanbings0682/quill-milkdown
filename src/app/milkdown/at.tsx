e20d14e043951e4efa2583258720cf0d23fb0bbf
hanbings <hanbings@whitepx.com>
2025-03-21 00:35:31 +0800

    [feat] add at type support.

--- src/app/milkdown/at.tsx
+++ src/app/milkdown/at.tsx
@@ -1,14 +1,62 @@
-import { MilkdownPlugin } from "@milkdown/kit/ctx";
+import { $inputRule, $node, $remark } from '@milkdown/kit/utils';
+import { InputRule } from '@milkdown/kit/prose/inputrules';
+import directive from 'remark-directive';
 
-export const atPlugin: MilkdownPlugin = (ctx) => {
-  // #1 prepare plugin
-  console.log("1")
-  return async () => {
-    // #2 run plugin
-    console.log("2")
-    return async () => {
-      // #3 clean up plugin
-      console.log("3")
-    };
-  };
-};
+const atHighlightDirective = $remark('remarkDirective', () => directive);
+
+const atDirectiveNode = $node('highlight', () => ({
+  group: 'inline',
+  inline: true,
+  attrs: {
+    text: { default: '' },
+  },
+  parseDOM: [{
+    tag: 'span[data-highlight]',
+    getAttrs: (dom) => ({
+      text: (dom as HTMLElement).textContent,
+    }),
+  }],
+  toDOM: (node) => [
+    'span',
+    {
+      'data-highlight': true,
+      style: `
+        color: #333; /* 字体颜色 */
+        background-color: #fff9c4; /* 柔和的黄色背景 */
+        border-radius: 4px; /* 圆角 */
+        padding: 2px 4px; /* 内边距 */
+        border: 1px solid #fff9c4; /* 细边框 */
+        box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.2); /* 轻微阴影 */
+        font-weight: 500; /* 字体稍微加粗 */
+      `,
+    },
+    `@${node.attrs.text}`,
+  ],
+  parseMarkdown: {
+    match: (node) => node.type === 'textDirective' && node.name === 'highlight',
+    runner: (state, node, type) => {
+      const text = node.children && node.children[0] && 'value' in node.children[0] 
+        ? node.children[0].value 
+        : ''; // 如果 children 为空，则默认空字符串
+      state.addNode(type, { text });
+    },
+  },
+  toMarkdown: {
+    match: (node) => node.type.name === 'highlight',
+    runner: (state, node) => {
+      state.addNode('textDirective', undefined, node.attrs.text, { name: 'highlight' });
+    },
+  }
+}));
+
+const atInputRule = $inputRule((ctx) => new InputRule(/@([^\n\r]+)/, (state, match, start, end) => {
+  const [okay, text = ''] = match;
+  const { tr } = state;
+  if (okay) {
+    tr.replaceWith(start, end, atDirectiveNode.type(ctx).create({ text }));
+  }
+
+  return tr;
+}));
+
+export { atHighlightDirective, atDirectiveNode, atInputRule };
--- src/app/milkdown/milkdown.tsx
+++ src/app/milkdown/milkdown.tsx
@@ -1,34 +1,62 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 
 import { Crepe } from "@milkdown/crepe";
 import { Milkdown, MilkdownProvider, useEditor } from "@milkdown/react";
 
 import "@milkdown/crepe/theme/common/style.css";
 import "@milkdown/crepe/theme/frame.css";
-import { atPlugin } from "./at";
+import "./milkdown-overrides.css";
+import { atDirectiveNode, atHighlightDirective, atInputRule } from "./at";
 
-const markdown = `# Milkdown React Crepe
-
-> You're scared of a world where you're needed.
-
-This is a demo for using Crepe with **React**.`;
+const markdown = "";
 
 export const MilkdownEditor: FC = () => {
   useEditor((root) => {
     const crepe = new Crepe({
       root,
       defaultValue: markdown,
+      features: {
+        [Crepe.Feature.ImageBlock]: false,
+        [Crepe.Feature.Table]: false,
+        [Crepe.Feature.LinkTooltip]: false,
+      },
+      featureConfigs: {
+        [Crepe.Feature.Placeholder]: {
+          text: '有什么想和大家分享的？',
+          mode: 'block',
+        },
+        [Crepe.Feature.BlockEdit]: {
+          slashMenuTextGroupLabel: "文字",
+          slashMenuListGroupLabel: "列表",
+          slashMenuAdvancedGroupLabel: "高级",
+          slashMenuH1Label: "标题",
+          slashMenuQuoteLabel: "引用",
+          slashMenuDividerLabel: "分割线",
+          slashMenuBulletListLabel: "无序列表",
+          slashMenuOrderedListLabel: "有序列表",
+          slashMenuTaskListLabel: "任务列表",
+          slashMenuCodeBlockLabel: "代码块",
+          slashMenuMathLabel: "数学公式",
+
+          buildMenu: (builder) => {
+            builder.getGroup("text").group.items = 
+              builder.getGroup("text").group.items
+                .filter((item) => item.key !== "h2")
+                .filter((item) => item.key !== "h3")
+                .filter((item) => item.key !== "h4")
+                .filter((item) => item.key !== "h5")
+                .filter((item) => item.key !== "h6")
+
+            builder.getGroup("advanced").group.items =
+              builder.getGroup("advanced").group.items
+                .filter((item) => item.key !== "image")
+                .filter((item) => item.key !== "table")
+          }
+        }
+      }
     })
-    
-    crepe.editor.use(atPlugin)
-    crepe.editor.action((ctx) => {
-      console.log("4")
-    })
-    crepe.on((lis) => {
-      lis.markdownUpdated((ctx, markdown, prev) => {
-        console.log("5")
-      })
-    })
+
+    crepe.editor.use([...atHighlightDirective, atDirectiveNode, atInputRule]);
 
     return crepe;
   }, []);
@@ -36,13 +64,12 @@ export const MilkdownEditor: FC = () => {
   return <Milkdown />;
 };
 
-
 export default function MilkdownEditorWrapper() {
   return (
-    <div className="h-96 w-[1280px]">
+    <div className="h-96 w-[960px]">
       <MilkdownProvider>
         <MilkdownEditor />
       </MilkdownProvider>
     </div>
-  );
+  )
 };
